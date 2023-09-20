ff68e0a6643015e52fbe7c2772ef99bd18e2ba69
kiyomaryu <ki4labo@gmail.com>
2023-09-21 08:32:48 +0900

    メインの描画まわりの処理を実装

--- pages-app/src/components/FieldView.vue
+++ pages-app/src/components/FieldView.vue
@@ -1,31 +1,41 @@
 <!-- テンプレート部分：ブラウザに表示されるとこ -->
 <template>
+	<!-- bootstrap-vueつかっているのでb-containerになっている -->
 	<b-container>
+		<!-- テキスト入力 -->
 		<b-row>
 			<b-col sm="1"><b-icon-file-text></b-icon-file-text></b-col>
 			<b-col><b-form-textarea rows="2" v-model="text"></b-form-textarea></b-col>
 		</b-row>
+		<!--	位置情報の表示	-->
 		<b-row v-if="lat && lon">
+			<!-- 緯度経度を表示 -->
 			<b-col sm="1"><b-icon-map></b-icon-map></b-col>
 			<b-col>Lat: {{lat}}} Lon:{{lon}}</b-col>
+			<!-- スポット一覧の表示 -->
 			<b-col>
 				<b-form-select v-model="location" :options="locations"></b-form-select>
 			</b-col>
 		</b-row>
+		<!-- 画像の選択 -->
 		<b-row>
 			<b-col sm="1"><b-icon-image></b-icon-image></b-col>
 			<b-col><b-form-file @change="onFileChange" accept="image/*" ref="fileInput" multiple placeholder="Selected files show in below"></b-form-file></b-col>
 		</b-row>
+		<!-- 画像のプレビュー -->
 		<b-row v-for="image in images" :key="image.idx">
 			<b-col sm="1"></b-col>
 			<b-col sm="3"><img :src="image.url" stype="max-width: 200px; max-height: 200px; object-fit: cover;"></b-col>
 			<b-col><b-button @click="removeImage(image.idx)"><b-icon-trash></b-icon-trash></b-button></b-col>
 		</b-row>
+		<!-- 各種ボタンの設置 -->
 		<b-row>
-			<b-col sm="1"><b-button @click="upload" :disabled="uploadState.disabled"><b-icon-pencil-square :animation="uploadState.animate"></b-icon-pencil-square></b-button></b-col>
-			<b-col sm="1"><b-button @click="fetch" :disabled="fetchState.disabled"><b-icon-pencil-square :animation="fetchState.animate"></b-icon-pencil-square></b-button></b-col>
 			<b-col sm="1">
-				<b-button @click="chageLocState" :disabled="locationState.disabled">
+				<b-button @click="upload" :disabled="uploadState.disabled"><b-icon-pencil-square :animation="uploadState.animate"></b-icon-pencil-square></b-button></b-col>
+			<b-col sm="1">
+				<b-button @click="fetch" :disabled="fetchState.disabled"><b-icon-pencil-square :animation="fetchState.animate"></b-icon-pencil-square></b-button></b-col>
+			<b-col sm="1">
+				<b-button @click="changeLocState" :disabled="locationState.disabled">
 					<b-icon-geo v-if="allowState.loc" :animation="locationState.animate"></b-icon-geo>
 					<b-iconstack v-else>
 						<b-icon-geo stacked></b-icon-geo><b-icon-stash-circle stacked scale="1.5"></b-icon-stash-circle>
@@ -34,11 +44,156 @@
 			</b-col>
 		</b-row>
 		<hr/>
-		</b-container>
+	</b-container>
 </template>
 
 <!-- ロジック部分：script書くところ -->
 <script lang="ts">
+// 外部モジュールやライブラリの読み込み
+// defineComponentはvueのコンポーネントを定義する関数
+import { defineComponent } from 'vue';
+import axios from 'axios'
+import { Param as ParamType, Image as ImageType } from '../types/Post'
+
+export default defineComponent({
+	name: 'FieldView',
+	// データの定義
+	data: () => {
+		return {
+			uploadState: {
+				disabled: false,
+				animate: ''
+			},
+			fetchState: {
+				disabled: false,
+				animate: ''
+			},
+			locationState: {
+				disabled: false,
+				animate: ''
+			},
+			text: '',
+			lat: '',
+			lon: '',
+			location: '---',
+			images: [] as ImageType[],
+			locations: ['---'],
+			allowState: {
+				loc: true
+			}
+		}
+	},
+	// createdはDOM生成前に実行されるアクション。ここでは位置情報と記事データを取得している
+	created: async function() {
+		this.getPosition()
+		await this.fetch()
+	},
+	// 独自メソッドの定義
+	methods: {
+		// ファイルが選択された場合のメソッド
+		// ファイルを一つ一つ確認して配列に画像データのURLを入れている、このタイミングで画像のプレビューが更新される
+		onFileChange(e: any) {
+			this.images = []
+			let idx = 0
+			for (const file of e.target.files) {
+				this.images.push({idx: idx, blob: file, url: URL.createObjectURL(file)})
+				idx++
+			}
+			(this.$refs.fileInput as any).reset()
+		},
+		// 位置情報を取得するメソッド
+		getPosition() {
+			this.locationState.animate = 'fade'
+			navigator.geolocation.getCurrentPosition((pos) => {
+				this.lat = pos.coords.latitude.toString()
+				this.lon = pos.coords.longitude.toString()
+				this.locationState.animate = ''
+				this.getLocations()
+			}, function (err) {
+				console.log(err)
+			},{
+				// 高精度な位置情報を取得するオプション
+				enableHighAccuracy: true,
+			});
+		},
+		// 近隣スポット一覧を取得するメソッド
+		// axiosでスポット一覧を取得するAPIにアクセスしている
+		async getLocations() {
+			if (this.lat = '' && this.lon == '') return;
+
+			const result = await axios.get(`${process.env.VUE_APP_ARTICLE_API}/nearby/${this.lat}/${this.lon}`)
+			this.locations = result.data.locations
+			this.locations.unshift('---')
+		},
+		// 記事を投稿するメソッド
+		// 画像ひとつひとつをアップロードしてIDを取得、変数に格納している
+		// 投稿後は投稿したデータIDの配列に追加して再描画の際は全体を取得せずにデータIDの画像だけを取得するようにして処理の負荷を軽減している
+		async upload(){
+			this.uploadState.animate = 'fade',
+			this.uploadState.disabled = true
+
+			let image_urls = []
+			for (const image of this.images) {
+				const formData = new FormData()
+				formData.append('image', image.blob)
+
+				let image_result = await axios.post(process.env.VUE_APP_UPLOAD_API + '/upload', formData,{
+					headers: {'Content-Type': 'multipart/form-data'}});
+
+				image_urls.push(image_result.data.id)
+			}
+
+			const postData = {
+				text: this.text,
+				lat: this.lat,
+				lon: this.lon,
+				location: this.location,
+				images: image_urls
+			} as ParamType
+
+			const axiosResponse = await axios.post(process.env.VUE_APP_ARTICLE_API + '/post', postData)
+			const {data} = axiosResponse;
+
+			(this.$parent as any).posts.unshift(data.id)
+
+			this.uploadState.animate = ''
+			this.uploadState.disabled = false
+			this.text = ''
+			this.location = '---'
+			this.locations = ['---']
+			this.images = []
+
+			this.getPosition()
+		},
+		// 記事一覧を取得するメソッド
+		async fetch() {
+			this.fetchState.animate = 'spin';
+			this.fetchState.disabled = true;
+
+			const result = await axios.get(process.env.VUE_APP_ARTICLE_API + '/');
+
+			(this.$parent as any).posts = result.data.data.map((val: any) => val.name).reverse();
+
+			this.fetchState.animate = '';
+			this.fetchState.disabled = false;
+		},
+		// 選択した画像を削除する処理。ボタン押すとその画像のインデックスが渡されて削除される
+		removeImage(idx: number) {
+			this.images = this.images.filter(image => image.idx != idx)
+		},
+		// 位置情報の取得を可否するかどうかの切り替え
+		changeLocState() {
+			this.allowState.loc = !this.allowState.loc
+
+			if (this.allowState.loc) {
+				this.getPosition();
+			} else {
+				this.lat = ''
+				this.lon = ''
+			}
+		}
+	}
+})
 </script>
 
 <!-- スタイル部分：css書くところ -->
